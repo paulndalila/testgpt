857f9c1e3403b1832a69f305acc7c8fe9c306341
Paul Ndalila <paulndalila001@gmail.com>
2023-09-14 14:47:41 +0300

    init

---
+++ Server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+
+const PORT = 8000;
+
+app.listen(PORT, "You server is running on port 3000...");
--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className='navbar'>
+          <h3>OpenAI APIs</h3>
+          <h2>textGPT</h2>
+          <button>&#x2B; New chat</button>
+        </div>
+
+        <div className='main'>
+          <div className='user'>
+            <p>User</p>
+            <p>hello</p>
+          </div>
+
+          <div className='assistant'>
+            <p>Assistant</p>
+            <p>Hello! How can I assist you today?</p>
+          </div>
+
+        </div>
+
+        <div className='bottom-section'>
+          <div className='input-field'>
+            <input type='text' name='text-input' placeholder='Entet text prompt here...' required/>
+            <button>Send</button>
+          </div>
+
+          <div>
+            <p>Paul's version of chatGPT. TestGPT may produce inaccurate information about people, places or facts. </p>
+            <p>Copywrite &copy;2023 Paul Ndalila</p>
+          </div>
+        </div>
     </div>
   );
 }
